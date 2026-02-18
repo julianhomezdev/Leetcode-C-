4bbff2da3fcaa382cc9424e316c58c7751d2daf6
Julian Homez <julianhomesdev@icloud.com>
2026-02-17 21:59:18 -0500

    Solved search insert with binary search O(log)n

--- Arrays/SearchInsert.cs
+++ Arrays/SearchInsert.cs
@@ -1,6 +1,90 @@
 using System.ComponentModel;
 
 public class Solution {
+
+    public int SearchInsertBinary(int[] nums, int target)
+    {
+        
+        // THIS IS BINARY SEARCH, FIX IT
+        
+        // [1, 3, 5, 6, 8, 9] -> 7
+
+        
+
+        // 0 + 7 = 7 / 2 -> 3
+
+        // nums[3] = 6
+
+        // 6 > 7 ?
+
+        // NO
+
+        // initial = middle +1 = 4
+
+        // end = 5
+
+        // middle = (4 + 5) // 2 = 4
+
+        // nums[4] = 8
+
+        // 8 > 7 = si
+
+        // end = middle -1  = 4 - 1 = 3
+
+        // initial = 4 
+
+        // end =  3
+
+        // middle = (4+3) / 2 = 3
+
+        // initial <= end ?? NOT
+
+        // RETURN INITIAL = 4
+
+        
+        // Is a sorted list (Check to be binary search)
+
+        // O(log) n
+
+
+        int initial = 0;
+        int end = nums.Length -1;
+
+        
+
+        while (initial <= end)
+        {
+            
+            int middle = (initial + end) / 2;
+
+            if (nums[middle] > target)
+            {
+                
+                end = middle -1;
+
+
+            } else if(nums[middle] < target)
+            {
+                
+
+                initial = middle +1;
+
+            } else
+            {
+                
+                return middle;
+
+            }
+
+        }
+
+        return initial;
+
+
+    }
+
+
+    // BAD SOLUTION
     public int SearchInsert(int[] nums, int target) {
         
         
@@ -46,6 +130,8 @@ public class Solution {
             }
             
         }
+
+        return 0;
         
     
         
